67ff86e27765daa768f720934d38bb169495b3cd
mohammed-sada <mohammedabusada94@example.com>
2021-04-17 13:33:14 +0300

    change

--- src/pages/LikedMovies.js
+++ src/pages/LikedMovies.js
@@ -10,7 +10,7 @@ const LikedMovies = () => {
     <div className="section liked-movies">
       <div>
         <Link to="/">
-          <button className="btn">back to home</button>
+          <button className="btn">back to hooome</button>
         </Link>
         <button
           style={{ margin: "10px" }}
